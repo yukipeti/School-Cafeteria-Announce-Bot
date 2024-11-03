a4e030e3398355909028cb26cf607d211df8ea61
yukipeti <ugachan0817@gmail.com>
2024-11-03 10:36:32 +0900

    明日の学食を表示させるコマンド

--- commands/tomorrow.js
+++ commands/tomorrow.js
@@ -6,22 +6,20 @@ const today = new Date().getDay();
 const dispMenu = ["Aセット", "Bセット", "カレー", "日替わりうどん・そば", "うどん・そば", "日替わりラーメン・パスタ", "ラーメン"];
 const menu = ["Aset", "Bset", "curry", "dailySpecialUdonSoba", "UdonSoba", "dailySpecialRamenPasta", "ramen"];
 
-
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("today")
-		.setDescription("今日の学食を表示します"),
+		.setName("tomorrow")
+		.setDescription("明日の学食を表示します"),
         async execute(interaction) {
-            if (today === 0 || today === 6) {
-                await interaction.reply("今日は土日です。学食はありません");
+            if ((today+1) === 0 || (today+1) === 6) {
+                await interaction.reply("明日は土日です。学食はありません");
                 return;
             }
-
             const embed = new EmbedBuilder()
-                .setTitle("今日の学食")
-                .setDescription("今日の学食は以下の通りです")
+                .setTitle("明日の学食")
+                .setDescription("明日の学食は以下の通りです")
                 .setFooter({text: "※内容は変更される可能性があります"})
-                .setTimestamp();
+                .setTimestamp()
                 dispMenu.forEach((item, index) => {
                     embed.addFields({
                         name: item,
